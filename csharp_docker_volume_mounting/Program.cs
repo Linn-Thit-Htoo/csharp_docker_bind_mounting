8f1abdd26ab06f19238100dc27de66258c91bb5b
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-24 20:35:45 +0630

    -

--- csharp_docker_volume_mounting/Program.cs
+++ csharp_docker_volume_mounting/Program.cs
@@ -2,8 +2,6 @@ using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
