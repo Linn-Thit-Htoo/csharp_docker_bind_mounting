507e3d1e4167d816ede97e06af0b72171b08ddfe
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-24 20:35:25 +0630

    -

--- csharp_docker_volume_mounting/Controllers/FileController.cs
+++ csharp_docker_volume_mounting/Controllers/FileController.cs
@@ -1,22 +1,21 @@
 ﻿using csharp_docker_volume_mounting.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace csharp_docker_volume_mounting.Controllers
+namespace csharp_docker_volume_mounting.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FileController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FileController : ControllerBase
+    [HttpPost("UploadFile")]
+    public async Task<IActionResult> UploadFileAsync(FileRequestModel requestModel, CancellationToken cs)
     {
-        [HttpPost("UploadFile")]
-        public async Task<IActionResult> UploadFileAsync(FileRequestModel requestModel, CancellationToken cs)
-        {
-            string fileName = $"{DateTime.UtcNow.ToString("yyyyMMdd_HHmmssfff")}_{requestModel.File.FileName}";
-            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Uploads", fileName);
+        string fileName = $"{DateTime.UtcNow.ToString("yyyyMMdd_HHmmssfff")}_{requestModel.File.FileName}";
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Uploads", fileName);
 
-            await using var stream = new FileStream(filePath, FileMode.Create);
-            await requestModel.File.CopyToAsync(stream, cs);
+        await using var stream = new FileStream(filePath, FileMode.Create);
+        await requestModel.File.CopyToAsync(stream, cs);
 
-            return Ok("Uploading Successful.");
-        }
+        return Ok("Uploading Successful.");
     }
 }
